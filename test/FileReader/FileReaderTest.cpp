020fb4463f39aa947e1afe6629dda2b1277bc7cb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-20 21:13:22 +0100

    test peekByte and check that it doesnt advance

--- test/FileReader/FileReaderTest.cpp
+++ test/FileReader/FileReaderTest.cpp
@@ -20,7 +20,7 @@ void writeFile(const std::string& path, const std::vector<uint8_t>& data)
 TEST(Read, ReadBytes)
 {
     const std::vector<uint8_t> data = {1, 4, 9, 240, 42, 67};
-    const std::string name = "dummy";
+    const std::string name = "takeByte";
 
     const std::string path = prefix + name;
     writeFile(path, data);
@@ -33,6 +33,40 @@ TEST(Read, ReadBytes)
         ASSERT_EQ(fr_takeByte(&r, &byte), Read_Ok);
         ASSERT_EQ(byte, expected);
     }
+    ASSERT_EQ(fr_takeByte(&r, &byte), Read_Done);
+
+    fr_close(&r);
+}
+
+TEST(Read, PeekByteDoesntAdvance)
+{
+    const std::vector<uint8_t> data = {0, 1};
+    const std::string name = "peekByte";
+
+    const std::string path = prefix + name;
+    writeFile(path, data);
+
+    FileReader r = fr_new(path.c_str());
+
+    uint8_t byte;
+    int nPeeks = 67;
+
+    for (int _ = 0; _ < nPeeks; ++_) {
+        ASSERT_EQ(fr_peekByte(&r, &byte), Read_Ok);
+        ASSERT_EQ(byte, 0);
+    }
+
+    ASSERT_EQ(fr_takeByte(&r, &byte), Read_Ok);
+    ASSERT_EQ(byte, 0);
+
+    for (int _ = 0; _ < nPeeks; ++_) {
+        ASSERT_EQ(fr_peekByte(&r, &byte), Read_Ok);
+        ASSERT_EQ(byte, 1);
+    }
+
+    ASSERT_EQ(fr_takeByte(&r, &byte), Read_Ok);
+    ASSERT_EQ(byte, 1);
+
     ASSERT_EQ(fr_takeByte(&r, &byte), Read_Done);
 
     fr_close(&r);
