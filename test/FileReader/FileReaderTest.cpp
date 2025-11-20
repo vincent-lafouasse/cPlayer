f5b69f266755c89ebf39529ec41926dace1e90d0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-20 21:07:58 +0100

    basic test

--- test/FileReader/FileReaderTest.cpp
+++ test/FileReader/FileReaderTest.cpp
@@ -1,8 +1,39 @@
+#include <cstdint>
+#include <fstream>
+
 #include "gtest/gtest.h"
 
+extern "C" {
 #include "FileReader.h"
+}
 
-TEST(Dummy, Dummy)
+const std::string prefix = "../build/test/";
+
+void writeFile(const std::string& path, const std::vector<uint8_t>& data)
 {
-    FileReader r;
+    std::ofstream os(path);
+    for (uint8_t byte : data) {
+        os << byte;
+    }
+}
+
+TEST(Read, ReadBytes)
+{
+    const std::vector<uint8_t> data = {0, 0};
+    const std::string name = "dummy";
+
+    const std::string path = prefix + name;
+    writeFile(path, data);
+
+    FileReader r = fr_new(path.c_str());
+
+    uint8_t byte;
+
+    for (uint8_t expected : data) {
+        ASSERT_EQ(fr_takeByte(&r, &byte), Read_Ok);
+        ASSERT_EQ(byte, expected);
+    }
+    ASSERT_EQ(fr_takeByte(&r, &byte), Read_Done);
+
+    fr_close(&r);
 }
