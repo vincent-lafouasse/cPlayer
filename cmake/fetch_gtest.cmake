114c277bb360ce1cabe739ade8cae2f8dd690c52
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-20 20:41:40 +0100

    update gtest

--- cmake/fetch_gtest.cmake
+++ cmake/fetch_gtest.cmake
@@ -2,6 +2,8 @@
 #
 # Targets : `libgtest` `libgmock`
 
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 if (CMAKE_VERSION VERSION_GREATER_EQUAL "3.24.0")
 	cmake_policy(SET CMP0135 NEW)
@@ -11,11 +13,12 @@ find_package(Threads REQUIRED)
 
 include(FetchContent)
 FetchContent_Declare(
-	googletest
-	GIT_REPOSITORY "https://github.com/google/googletest.git"
-	GIT_TAG "main"
-)
-
+        googletest
+        URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
+        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+        DOWNLOAD_NO_PROGRESS TRUE
+      )
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
 
