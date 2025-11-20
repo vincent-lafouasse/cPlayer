eedb4bf79c438fcaaf4848652dec420d84773b0e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 00:41:08 +0100

    parse RIFF chunk ID

--- src/main.c
+++ src/main.c
@@ -1,22 +1,25 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "FileReader.h"
 
 int main(void)
 {
-    const char* path = "./src/main.c";
+    const char* path = "./wav/f1_32bit.wav";
     FileReader reader = fr_open(path);
     if (reader.fd == -1) {
         printf("Failed to open file %s\n", path);
         exit(1);
     }
 
-    uint8_t byte;
-    ReadResult res;
-    while ((res = fr_takeByte(&reader, &byte)) == Read_Ok) {
-        printf("%c", byte);
-    }
-    printf("%s\n", rr_repr(res));
+    uint8_t chunkID[4];
+    fr_takeByte(&reader, chunkID);
+    fr_takeByte(&reader, chunkID + 1);
+    fr_takeByte(&reader, chunkID + 2);
+    fr_takeByte(&reader, chunkID + 3);
+    assert(strncmp((const char*)chunkID, "RIFF", 4) == 0);
+
     fr_close(&reader);
 }
