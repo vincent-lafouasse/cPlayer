c64fcd6119238970628ac2a43fbf7c06726b709e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 23:00:04 +0100

    log with color

--- src/common/log.h
+++ src/common/log.h
@@ -3,6 +3,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#define Ansi_Black "\x1b[0;30m"
+#define Ansi_Red "\x1b[0;31m"
+#define Ansi_Green "\x1b[0;32m"
+#define Ansi_Yellow "\x1b[0;33m"
+#define Ansi_Blue "\x1b[0;34m"
+#define Ansi_Purple "\x1b[0;35m"
+#define Ansi_Cyan "\x1b[0;36m"
+#define Ansi_White "\x1b[0;37m"
+#define Ansi_Reset "\x1b[0m"
+
 #define LOGGING 1
 
 typedef enum {
@@ -16,15 +26,15 @@ static inline const char* logLevelStr(LogLevel level)
 {
     switch (level) {
         case Error:
-            return "Error";
+            return Ansi_Red "[Error]" Ansi_Reset;
         case Warning:
-            return "Warning";
+            return Ansi_Yellow "[Warning]" Ansi_Reset;
         case Info:
-            return "Info";
+            return Ansi_Green "[Info]" Ansi_Reset;
         case Debug:
-            return "Debug";
+            return Ansi_Blue "[Debug]" Ansi_Reset;
         default:
-            return "?";
+            return "[?]";
     }
 }
 
@@ -34,7 +44,7 @@ static inline void logFn(LogLevel level, const char* fmt, ...)
 {
 #if LOGGING
     if (level <= GLOBAL_LOG_LEVEL_THRESHOLD) {
-        fprintf(stderr, "[%s]\t", logLevelStr(level));
+        fprintf(stderr, "%s\t", logLevelStr(level));
 
         va_list args;
         va_start(args, fmt);
