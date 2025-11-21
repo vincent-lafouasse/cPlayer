9fc61febbdc7212158d487ffe2d5a9aa687055e0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 02:29:53 +0100

    test reading a u16 past the end of the buffer

--- test/FileReader/FileReaderTest.cpp
+++ test/FileReader/FileReaderTest.cpp
@@ -10,6 +10,7 @@ extern "C" {
 
 const std::string prefix = "../build/test/";
 const std::string suffix = ".dat";
+static constexpr std::size_t buffer_size = BUFFER_SIZE;
 
 void writeFile(const std::string& path, const std::vector<uint8_t>& data)
 {
@@ -19,8 +20,7 @@ void writeFile(const std::string& path, const std::vector<uint8_t>& data)
     }
 }
 
-[[maybe_unused]]
-void logReader(const FileReader& r)
+[[maybe_unused]] void logReader(const FileReader& r)
 {
     std::cerr << std::format("FileReader {{\n");
     std::cerr << std::format("\tfd:\t{}\n", r.fd);
@@ -165,6 +165,48 @@ TEST(Read, TakeU16_DoesntAdvanceOnReadError)
     ASSERT_EQ(fr_takeU16LE(&r, &actual), Read_Err);
     ASSERT_EQ(fr_takeU16LE(&r, &actual), Read_Err);
     ASSERT_EQ(r.head, static_cast<std::size_t>(0));
+
+    fr_close(&r);
+}
+
+TEST(Read, TakeU16_PastBuffer)
+{
+    const std::string name = "takeU16_PastBuffer";
+
+    std::vector<uint8_t> data;
+    for (std::size_t i = 0; i < buffer_size - 1; ++i) {
+        data.push_back(67);
+    }
+    data.push_back(0x34);
+    data.push_back(0x12);
+
+    const std::string path = prefix + name + suffix;
+    writeFile(path, data);
+
+    FileReader r = fr_open(path.c_str());
+    if (!fr_isOpened(&r)) {
+        FAIL() << std::format("Failed to open file {}", path);
+    }
+
+    for (std::size_t i = 0; i < buffer_size - 1; ++i) {
+        uint8_t byte;
+        const auto res = fr_takeByte(&r, &byte);
+        ASSERT_EQ(res, Read_Ok);
+        ASSERT_EQ(byte, 67);
+    }
+    ASSERT_EQ(r.head, buffer_size - 1);
+
+    const uint16_t expected = 0x1234;
+    uint16_t actual;
+
+    ASSERT_EQ(fr_peekU16LE(&r, &actual), Read_Ok);
+    ASSERT_EQ(actual, expected);
+
+    ASSERT_EQ(fr_takeU16LE(&r, &actual), Read_Ok);
+    ASSERT_EQ(actual, expected);
+
+    uint8_t byte;
+    ASSERT_NE(fr_peekByte(&r, &byte), Read_Ok);
 
     fr_close(&r);
 }
