f0b05a77191a26e78ec66fe1a9faae8dff8ff36c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 16:58:57 +0100

    write i24 tests

--- test/i24/i24Test.cpp
+++ test/i24/i24Test.cpp
@@ -4,8 +4,12 @@ extern "C" {
 #include "int24.h"
 }
 
-TEST(i24, Dummy)
+TEST(i24, PositiveNumbers)
 {
-    const Int24 i{0, 0, 0};
-    EXPECT_EQ(i24_asI32(i), 0);
+    EXPECT_EQ(i24_asI32(Int24{0, 0, 0}), 0x0);
+    EXPECT_EQ(i24_asI32(Int24{0, 0, 1}), 0x1);
+    EXPECT_EQ(i24_asI32(Int24{0, 1, 0}), 0x100);
+    EXPECT_EQ(i24_asI32(Int24{1, 0, 0}), 0x10000);
+    EXPECT_EQ(i24_asI32(Int24{0x11, 0x11, 0x11}), 0x111111);
+    EXPECT_EQ(i24_asI32(Int24{0x7f, 0xff, 0xff}), 0x007fffff);
 }
