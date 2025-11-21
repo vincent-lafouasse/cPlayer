98692dc5b0d9c9f782f0b8f9bf4c5aad1085979d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 17:43:06 +0100

    i24 tests

--- test/i24/i24Test.cpp
+++ test/i24/i24Test.cpp
@@ -4,17 +4,63 @@ extern "C" {
 #include "int24.h"
 }
 
-TEST(i24, PositiveNumbers)
+// for convenience
+int32_t i24(uint8_t high, uint8_t mid, uint8_t low)
 {
-    EXPECT_EQ(i24_asI32(Int24{0, 0, 0}), 0x0);
-    EXPECT_EQ(i24_asI32(Int24{0, 0, 1}), 0x1);
-    EXPECT_EQ(i24_asI32(Int24{0, 1, 0}), 0x100);
-    EXPECT_EQ(i24_asI32(Int24{1, 0, 0}), 0x10000);
-    EXPECT_EQ(i24_asI32(Int24{0x11, 0x11, 0x11}), 0x111111);
-    EXPECT_EQ(i24_asI32(Int24{0x7f, 0xff, 0xff}), 0x007fffff);
+    return i24_asI32(Int24{high, mid, low});
 }
 
-TEST(i24, NegativeNumbers)
+TEST(i24, Zero)
 {
-    EXPECT_EQ(i24_asI32(Int24{0xff, 0xff, 0xff}), -1);
+    EXPECT_EQ(i24(0x00, 0x00, 0x00), 0);
+}
+
+TEST(i24, PositiveSimpleValues)
+{
+    EXPECT_EQ(i24(0x00, 0x00, 0x01), 1);
+    EXPECT_EQ(i24(0x00, 0x00, 0x7f), 127);
+    EXPECT_EQ(i24(0x00, 0x01, 0x00), 256);
+    EXPECT_EQ(i24(0x00, 0x10, 0x00), 4096);
+}
+
+TEST(i24, PositiveMax)
+{
+    EXPECT_EQ(i24(0x7f, 0xff, 0xff), 0x7fffff);
+}
+
+TEST(i24, NegativeSimpleValues)
+{
+    // -1 in i24 = 0xffffff
+    EXPECT_EQ(i24(0xff, 0xff, 0xff), -1);
+
+    // -128 in i24 = 0xffff80
+    EXPECT_EQ(i24(0xff, 0xff, 0x80), -128);
+
+    // -256 in i24 = 0xffff00
+    EXPECT_EQ(i24(0xff, 0xff, 0x00), -256);
+}
+
+TEST(i24, NegativeMidRange)
+{
+    // -32768 = 0xff8000
+    EXPECT_EQ(i24(0xff, 0x80, 0x00), -32768);
+
+    // -1000000 = 0xf0bdc0
+    EXPECT_EQ(i24(0xf0, 0xbd, 0xc0), -1000000);
+}
+
+TEST(i24, NegativeMin)
+{
+    // Minimum signed 24-bit value: 0x800000 = -8,388,608
+    EXPECT_EQ(i24(0x80, 0x00, 0x00), -8388608);
+}
+
+// Boundary-crossing tests around sign bit
+TEST(i24, SignBoundary)
+{
+    // Value just below sign bit (positive)
+    EXPECT_EQ(i24(0x7f, 0xff, 0xfe), 8388606);
+
+    // Value where sign bit just flips (negative)
+    EXPECT_EQ(i24(0x80, 0x00, 0x01), -8388607);
 }
