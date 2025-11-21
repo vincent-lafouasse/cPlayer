782d47c4361c96e873b9a570b3aeab4f9f5ffb2d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 17:34:59 +0100

    parse i24

--- src/int24.c
+++ src/int24.c
@@ -1,34 +1,12 @@
 #include "int24.h"
 
-typedef uint8_t u8;
-typedef Int24 i24;
-
-typedef uint32_t u32;
-typedef int32_t i32;
-
-static i32 combine(u8 b3, u8 b2, u8 b1, u8 b0)
+int32_t i24_asI32(Int24 i)
 {
-    u32 byte3 = b3;
-    u32 byte2 = b2;
-    u32 byte1 = b1;
-    u32 byte0 = b0;
+    int32_t value = (i.highByte << 16) | (i.middleByte << 8) | i.lowByte;
 
-    u32 bits = (byte3 << 24) + (byte2 << 16) + (byte1 << 8) + byte0;
-    i32* p = (i32*)(&bits);
-    return *p;
-}
+    // extend sign bit
+    if (value & 0x00800000)
+        value |= 0xFF000000;
 
-i32 i24_asI32(i24 i)
-{
-    u8 highByte = i.highByte;
-    u8 middleByte = i.middleByte;
-    u8 lowByte = i.lowByte;
-
-    // get sign bit into sign byte (top byte)
-    u8 signByte = highByte & (1 << 7);
-
-    // remove sign bit from the high byte
-    highByte &= ~(1 << 7);
-
-    return combine(signByte, highByte, middleByte, lowByte);
+    return value;
 }
