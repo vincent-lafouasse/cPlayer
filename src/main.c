5f23e16ca31239e1ca7f30ae919f850eaa9f9b9e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-21 17:49:18 +0100

    implement readI24

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "FileReader.h"
+#include "int24.h"
 
 #include "log.h"
 
@@ -42,6 +43,19 @@ ReadResult readFourCC(FileReader* reader, uint8_t* out)
             return res;
         }
     }
+    return Read_Ok;
+}
+
+ReadResult readI24AsI32LE(FileReader* reader, int32_t* out)
+{
+    uint8_t bytes[3];
+    for (size_t i = 0; i < 4; ++i) {
+        const ReadResult res = fr_takeByte(reader, bytes + i);
+        if (res != Read_Ok) {
+            return res;
+        }
+    }
+    *out = i24_asI32((Int24){bytes[2], bytes[1], bytes[0]});
     return Read_Ok;
 }
 
