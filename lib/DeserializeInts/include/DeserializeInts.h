13fa9b2a6f386c7c032c936baf25726a3800eb86
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-22 01:19:14 +0100

    implement deserializeU16_*()

--- lib/DeserializeInts/include/DeserializeInts.h
+++ lib/DeserializeInts/include/DeserializeInts.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <stdint.h>
+
+typedef uint8_t Byte;
+
+// you better give me 2 bytes or else
+uint16_t deserializeU16_LE(const Byte* bytes);
+uint16_t deserializeU16_BE(const Byte* bytes);
--- lib/DeserializeInts/src/DeserializeInts.c
+++ lib/DeserializeInts/src/DeserializeInts.c
@@ -1,1 +1,15 @@
 #include "DeserializeInts.h"
+
+uint16_t deserializeU16_LE(const Byte* bytes)
+{
+	const uint16_t low = bytes[0];
+	const uint16_t high = bytes[1];
+	return (high << 8) | low;
+}
+
+uint16_t deserializeU16_BE(const Byte* bytes)
+{
+	const uint16_t high = bytes[0];
+	const uint16_t low = bytes[1];
+	return (high << 8) | low;
+}
