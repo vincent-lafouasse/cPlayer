3b531f7e85209228fbe61024848fb5abaeca640f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-22 02:12:58 +0100

    more tests

--- test/DeserializeInts/DeserializeIntsTest.cpp
+++ test/DeserializeInts/DeserializeIntsTest.cpp
@@ -5,6 +5,7 @@ extern "C" {
 }
 
 using u16 = uint16_t;
+using i16 = int16_t;
 using u32 = uint32_t;
 
 TEST(Deserialize, U16_LE)
@@ -120,5 +121,73 @@ TEST(Deserialize, U32_BE)
         constexpr Byte b[4] = {0x12, 0x34, 0x56, 0x78};
         constexpr u32 expected = 0x12345678;
         EXPECT_EQ(deserializeU32_BE(b), expected);
+    }
+}
+
+TEST(Deserialize, I16_LE)
+{
+    {
+        constexpr Byte b[2] = {0x00, 0x00};
+        constexpr i16 expected = 0x0000;
+        EXPECT_EQ(deserializeI16_LE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0x01, 0x00};
+        constexpr i16 expected = 0x0001;
+        EXPECT_EQ(deserializeI16_LE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0xff, 0x7f};
+        constexpr i16 expected = INT16_MAX;
+        EXPECT_EQ(deserializeI16_LE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0x00, 0x80};
+        constexpr i16 expected = INT16_MIN;
+        EXPECT_EQ(deserializeI16_LE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0xfe, 0xca};
+        constexpr i16 expected = -13570;  // 0xcafe
+        EXPECT_EQ(deserializeI16_LE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0xff, 0xff};
+        constexpr i16 expected = -1;
+        EXPECT_EQ(deserializeI16_LE(b), expected);
+    }
+}
+
+TEST(Deserialize, I16_BE)
+{
+    {
+        constexpr Byte b[2] = {0x00, 0x00};
+        constexpr i16 expected = 0x0000;
+        EXPECT_EQ(deserializeI16_BE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0x00, 0x01};
+        constexpr i16 expected = 0x0001;
+        EXPECT_EQ(deserializeI16_BE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0x7f, 0xff};
+        constexpr i16 expected = INT16_MAX;
+        EXPECT_EQ(deserializeI16_BE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0x80, 0x00};
+        constexpr i16 expected = INT16_MIN;
+        EXPECT_EQ(deserializeI16_BE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0xca, 0xfe};
+        constexpr i16 expected = -13570;  // 0xcafe
+        EXPECT_EQ(deserializeI16_BE(b), expected);
+    }
+    {
+        constexpr Byte b[2] = {0xff, 0xff};
+        constexpr i16 expected = -1;
+        EXPECT_EQ(deserializeI16_BE(b), expected);
     }
 }
