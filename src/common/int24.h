6a0cc5e1df3e0769cc6912d22bda981a851d981a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-22 01:40:37 +0100

    remove unnecessary include

--- src/common/int24.h
+++ src/common/int24.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <stdint.h>
-#include "DeserializeInts.h"
 
 #define INT32_UNIT ((int32_t)1)
 #define INT24_MAX ((INT32_UNIT << 23) - 1)
@@ -13,5 +12,6 @@ typedef struct {
     uint8_t lowByte;
 } Int24;
 
-Int24 deserializeI24_LE(const Byte* bytes);
+// you better give me 3 bytes or else
+Int24 deserializeI24_LE(const uint8_t* bytes);
 int32_t i24_asI32(Int24 i);
