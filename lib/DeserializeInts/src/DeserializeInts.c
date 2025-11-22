818a41ba240b4a7fe0216aa2ae85385b4f3135d9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-22 01:25:13 +0100

    implement deserializeU32()

--- lib/DeserializeInts/src/DeserializeInts.c
+++ lib/DeserializeInts/src/DeserializeInts.c
@@ -13,3 +13,21 @@ uint16_t deserializeU16_BE(const Byte* bytes)
 	const uint16_t low = bytes[1];
 	return (high << 8) | low;
 }
+
+uint32_t deserializeU32_LE(const Byte* bytes)
+{
+	const uint32_t low = bytes[0];
+	const uint32_t lowish = bytes[1];
+	const uint32_t highish = bytes[2];
+	const uint32_t high = bytes[3];
+	return (high << 24) | (highish << 16) | (lowish << 8) | low;
+}
+
+uint32_t deserializeU32_BE(const Byte* bytes)
+{
+	const uint32_t high = bytes[0];
+	const uint32_t highish = bytes[1];
+	const uint32_t lowish = bytes[2];
+	const uint32_t low = bytes[3];
+	return (high << 24) | (highish << 16) | (lowish << 8) | low;
+}
