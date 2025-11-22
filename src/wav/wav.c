0a976a18748ce6debfadeb8a26d79a191591faac
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-11-22 03:39:57 +0100

    refactor

--- src/wav/wav.c
+++ src/wav/wav.c
@@ -15,14 +15,24 @@ AudioData decodeWav(FileReader* reader)
 AudioData readWavData(FileReader* reader, Header h)
 {
     float* left = malloc(h.size * sizeof(float));
-    float* right = malloc(h.size * sizeof(float));
+    float* right;
+    if (h.nChannels > 1) {
+        right = malloc(h.size * sizeof(float));
+    } else {
+        right = NULL;
+    }
 
     for (uint32_t i = 0; i < h.size; ++i) {
-        (void)readSample(reader, Signed24, left + i);
-        (void)readSample(reader, Signed24, right + i);
+        (void)readSample(reader, h.sampleFormat, left + i);
+        if (h.nChannels > 1) {
+            (void)readSample(reader, h.sampleFormat, right + i);
+        }
     }
     dumpFloatCsv(left, h.size, DUMP_PREFIX "float" DUMP_SUFFIX);
 
+    if (h.nChannels == 1) {
+        right = left;
+    }
     return (AudioData){.left = left,
                        .right = right,
                        .size = h.size,
