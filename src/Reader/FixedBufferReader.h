2de9025f64420bec7f9686618f2a9965d19a9370
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-03 16:28:19 +0100

    restructure

---
+++ src/Reader/FixedBufferReader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define FIXED_BUFFER_SIZE 4096
+
+typedef struct {
+    uint8_t buffer[FIXED_BUFFER_SIZE];
+    size_t head;
+} FixedBufferReader;
--- src/Reader/Reader.h
+++ src/Reader/Reader.h
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include "FileReader.h"
+#include "FixedBufferReader.h"
 
 typedef struct Reader Reader;
 
@@ -19,74 +20,5 @@ struct Reader {
     size_t offset;
 };
 
-bool readFromFileReader(Reader* reader, uint8_t* buffer, size_t n)
-{
-    FileReader* fileReader = reader->ctx;
-
-    SliceResult slice = fr_takeSlice(fileReader, n);
-    if (slice.status != ReadStatus_Ok) {
-        return false;
-    }
-
-    memcpy(buffer, slice.slice, slice.len);
-    reader->offset += slice.len;
-    return true;
-}
-
-bool skipFromFileReader(Reader* reader, size_t n)
-{
-    FileReader* fileReader = reader->ctx;
-
-    SliceResult slice = fr_takeSlice(fileReader, n);
-
-    return slice.status == ReadStatus_Ok;
-}
-
-Reader reader_fromFileReader(FileReader* fileReader)
-{
-    return (Reader){.ctx = fileReader,
-                    .read = readFromFileReader,
-                    .skip = skipFromFileReader,
-                    .offset = 0};
-}
-
-#define FIXED_BUFFER_SIZE 4096
-
-typedef struct {
-    uint8_t buffer[FIXED_BUFFER_SIZE];
-    size_t head;
-} FixedBufferReader;
-
-bool readFromFixedBufferReader(Reader* reader, uint8_t* buffer, size_t n)
-{
-    FixedBufferReader* fbReader = reader->ctx;
-
-    if (fbReader->head + n < FIXED_BUFFER_SIZE) {
-        return false;
-    }
-
-    memcpy(buffer, fbReader->buffer + fbReader->head, n);
-    reader->offset += n;
-    fbReader->head += n;
-    return true;
-}
-
-bool skipFromFixedBufferReader(Reader* reader, size_t n)
-{
-    FixedBufferReader* fbReader = reader->ctx;
-
-    if (fbReader->head + n < FIXED_BUFFER_SIZE) {
-        return false;
-    }
-
-    fbReader->head += n;
-    return true;
-}
-
-Reader readerFromFixedBufferReader(FixedBufferReader* fbReader)
-{
-    return (Reader){.ctx = fbReader,
-                    .read = readFromFixedBufferReader,
-                    .skip = skipFromFixedBufferReader,
-                    .offset = 0};
-}
+Reader reader_fromFileReader(FileReader* fileReader);
+Reader readerFromFixedBufferReader(FixedBufferReader* fbReader);
