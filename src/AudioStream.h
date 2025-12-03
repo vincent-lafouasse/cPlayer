35a677de7a9f4afc20a4a0c65223df9200cd4f64
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-03 12:50:39 +0100

    format

--- src/AudioStream.h
+++ src/AudioStream.h
@@ -5,60 +5,60 @@
 #include <portaudio.h>
 
 #include "Error.h"
-#include "log.h" // TODO: this should not be here
+#include "log.h"  // TODO: this should not be here
 
 typedef struct {
-	uint32_t nChannels;
-	uint32_t sampleRate; 
+    uint32_t nChannels;
+    uint32_t sampleRate;
 } StreamConfig;
 
 typedef struct {
-	StreamConfig cfg;
-	PaStream* paStream;
+    StreamConfig cfg;
+    PaStream* paStream;
 } AudioStream;
 
 void stream_initBackend(void)
 {
-	// TODO: return Error
-	Pa_Initialize();
+    // TODO: return Error
+    Pa_Initialize();
 }
 
 void stream_deinitBackend(void)
 {
-	// TODO: return Error
-	Pa_Terminate();
+    // TODO: return Error
+    Pa_Terminate();
 }
 
 AudioStream stream_loadStereo(StreamConfig cfg)
 {
-	// TODO: return a Result instead
-	const PaDeviceIndex device = Pa_GetDefaultOutputDevice();
-	if (device == paNoDevice) {
-		logFn(LogLevel_Error, "No output device.\n");
-		return 1;
-	}
+    // TODO: return a Result instead
+    const PaDeviceIndex device = Pa_GetDefaultOutputDevice();
+    if (device == paNoDevice) {
+        logFn(LogLevel_Error, "No output device.\n");
+        return 1;
+    }
 
-	const PaStreamParameters outParams = (PaStreamParameters){
-		.device = device,
-		.channelCount = cfg.nChannels,
-		.sampleFormat = paFloat32,
-		.suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowOutputLatency,
-		.hostApiSpecificStreamInfo = NULL,
-	};
+    const PaStreamParameters outParams = (PaStreamParameters){
+        .device = device,
+        .channelCount = cfg.nChannels,
+        .sampleFormat = paFloat32,
+        .suggestedLatency = Pa_GetDeviceInfo(device)->defaultLowOutputLatency,
+        .hostApiSpecificStreamInfo = NULL,
+    };
 }
 
 void stream_close(AudioStream* stream)
 {
-	// TODO: return Error
-	Pa_CloseStream(stream->paStream);
+    // TODO: return Error
+    Pa_CloseStream(stream->paStream);
 }
 
 void stream_start(AudioStream* stream)
 {
-	Pa_StartStream(stream->stream);
+    Pa_StartStream(stream->stream);
 }
 
 void stream_stop(AudioStream* stream)
 {
-	Pa_CloseStream(stream->stream);
+    Pa_CloseStream(stream->stream);
 }
