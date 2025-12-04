0f0cc8d7481f6b9f1cf4ec9243d0135a3222b697
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-04 21:02:09 +0100

    more tests

--- test/Wav/WavHeaderTest.cpp
+++ test/Wav/WavHeaderTest.cpp
@@ -56,3 +56,53 @@ TEST(WavReader, SkipChunkUntil_UnexpectedEOF)
 
     EXPECT_EQ(skipChunkUntil(&r, "fmt "), E_UnexpectedEOF);
 }
+
+TEST(WavReader, SkipChunkUntil_MultipleJunk)
+{
+    std::vector<Byte> data;
+    appendChunk(data, "JUNK", 2, {0xAA, 0xBB});
+    appendChunk(data, "FAKE", 4, {0x01, 0x02, 0x03, 0x04});
+    appendChunk(data, "TRSH", 1, {0x00});
+    appendChunk(data, "fmt ", 16, std::vector<Byte>(16, 0x22));
+    MemoryReader mem(data);
+    Reader r = memoryReaderInterface(&mem);
+
+    ASSERT_EQ(skipChunkUntil(&r, "fmt "), NoError);
+
+    Slice s;
+    ASSERT_EQ(reader_takeSlice(&r, 4, &s), NoError);
+    EXPECT_EQ(std::string((char*)s.slice, s.len), "fmt ");
+}
+
+TEST(WavReader, SkipChunkUntil_ImmediateFmt)
+{
+    std::vector<Byte> data;
+    appendChunk(data, "fmt ", 16, std::vector<Byte>(16, 0x33));
+    MemoryReader mem(data);
+    Reader r = memoryReaderInterface(&mem);
+
+    ASSERT_EQ(skipChunkUntil(&r, "fmt "), NoError);
+    Slice s;
+    ASSERT_EQ(reader_takeSlice(&r, 4, &s), NoError);
+    EXPECT_EQ(std::string((char*)s.slice, s.len), "fmt ");
+}
+
+TEST(WavReader, SkipChunkUntil_EmptyReader)
+{
+    std::vector<Byte> data;  // empty
+    MemoryReader mem(data);
+    Reader r = memoryReaderInterface(&mem);
+    EXPECT_EQ(skipChunkUntil(&r, "fmt "), E_UnexpectedEOF);
+}
+TEST(WavReader, SkipChunkUntil_MalformedSize)
+
+{
+    std::vector<Byte> data;
+    appendChunk(data, "JUNK", 0xFFFFFFF0, {});  // huge size (beyond buffer)
+    appendChunk(data, "fmt ", 16, std::vector<Byte>(16, 0x44));
+    MemoryReader mem(data);
+    Reader r = memoryReaderInterface(&mem);
+
+    // Should fail due to trying to skip beyond EOF
+    EXPECT_EQ(skipChunkUntil(&r, "fmt "), E_UnexpectedEOF);
+}
