e1281d1321483dcaa32c39f377963f33a7dbfcec
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-04 16:12:10 +0100

    implement tests

--- test/Reader/ReaderTest.cpp
+++ test/Reader/ReaderTest.cpp
@@ -62,7 +62,77 @@ class MemoryReader {
     }
 };
 
-TEST(Reader, Dummy)
+Error memoryReaderPeekSlice(Reader* reader, size_t n, Slice* out)
 {
-    FAIL();
+    MemoryReader* memoryReader = static_cast<MemoryReader*>(reader->ctx);
+
+    return memoryReader->peekSlice(n, out);
+}
+
+Error memoryReaderPeekInto(Reader* reader, size_t n, uint8_t* out)
+{
+    MemoryReader* memoryReader = static_cast<MemoryReader*>(reader->ctx);
+
+    return memoryReader->peekInto(n, out);
+}
+
+Error memoryReaderSkip(Reader* reader, size_t n)
+{
+    MemoryReader* memoryReader = static_cast<MemoryReader*>(reader->ctx);
+
+    Error err = memoryReader->skip(n);
+    if (err != NoError) {
+        return err;
+    } else {
+        reader->offset += n;
+        return NoError;
+    }
+}
+
+Reader memoryReaderInterface(MemoryReader* memoryReader)
+{
+    return {
+        .ctx = static_cast<void*>(memoryReader),
+        .peekSlice = memoryReaderPeekSlice,
+        .peekInto = memoryReaderPeekInto,
+        .skip = memoryReaderSkip,
+        .offset = 0,
+    };
+}
+
+void assertSliceEq(const Slice& slice, const std::string& expected)
+{
+    std::string actual(slice.slice, slice.slice + slice.len);
+    ASSERT_EQ(actual, expected);
+}
+
+TEST(Reader, PeekSlice)
+{
+    MemoryReader memoryReader("1234567");
+    Reader reader = memoryReaderInterface(&memoryReader);
+
+    Slice slice;
+
+    ASSERT_EQ(reader.peekSlice(&reader, 1, &slice), NoError);
+    assertSliceEq(slice, "1");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 2, &slice), NoError);
+    assertSliceEq(slice, "12");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 3, &slice), NoError);
+    assertSliceEq(slice, "123");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 4, &slice), NoError);
+    assertSliceEq(slice, "1234");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 5, &slice), NoError);
+    assertSliceEq(slice, "12345");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 6, &slice), NoError);
+    assertSliceEq(slice, "123456");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 7, &slice), NoError);
+    assertSliceEq(slice, "1234567");
+
+    ASSERT_EQ(reader.peekSlice(&reader, 8, &slice), E_UnexpectedEOF);
 }
