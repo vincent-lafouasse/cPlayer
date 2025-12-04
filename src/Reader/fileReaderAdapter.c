69d0a1fb8fdaaceb1f60baf4de0c37d9a3c52ec5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-04 16:56:27 +0100

    implement FileReader adapter

--- src/Reader/fileReaderAdapter.c
+++ src/Reader/fileReaderAdapter.c
@@ -1,32 +1,61 @@
+#include "Error.h"
+#include "FileReader.h"
 #include "ReaderAdapters.h"
 
 #include <string.h>
 
-static bool readFromFileReader(Reader* reader, uint8_t* buffer, size_t n)
+static Error reader_FileReaderPeekSlice(Reader* reader, size_t n, Slice* out)
 {
-    FileReader* fileReader = reader->ctx;
+    FileReader* fileReader = (FileReader*)reader->ctx;
 
-    SliceResult slice = fr_takeSlice(fileReader, n);
-    if (slice.status != ReadStatus_Ok) {
-        return false;
+    SliceResult slice = fr_peekSlice(fileReader, n);
+    if (slice.status == ReadStatus_ReadErr) {
+        return E_FailedRead;
+    } else if (slice.status == ReadStatus_EOF) {
+        return E_UnexpectedEOF;
     }
 
-    memcpy(buffer, slice.slice, slice.len);
-    reader->offset += slice.len;
-    return true;
+    *out = (Slice){.slice = slice.slice, .len = slice.len};
+    return NoError;
 }
 
-static bool skipFromFileReader(Reader* reader, size_t n)
+static Error reader_FileReaderPeekInto(Reader* reader, size_t n, uint8_t* out)
 {
-    FileReader* fileReader = reader->ctx;
+    FileReader* fileReader = (FileReader*)reader->ctx;
 
-    SliceResult slice = fr_takeSlice(fileReader, n);
+    SliceResult slice = fr_peekSlice(fileReader, n);
+    if (slice.status == ReadStatus_ReadErr) {
+        return E_FailedRead;
+    } else if (slice.status == ReadStatus_EOF) {
+        return E_UnexpectedEOF;
+    }
 
-    return slice.status == ReadStatus_Ok;
+    memcpy(out, slice.slice, slice.len);
+    return NoError;
+}
+
+static Error reader_FileReaderSkip(Reader* reader, size_t n)
+{
+    FileReader* fileReader = (FileReader*)reader->ctx;
+
+    ReadStatus status = fr_skip(fileReader, n);
+    if (status == ReadStatus_ReadErr) {
+        return E_FailedRead;
+    } else if (status == ReadStatus_EOF) {
+        return E_UnexpectedEOF;
+    } else {
+        reader->offset += n;
+        return NoError;
+    }
 }
 
 Reader reader_fromFileReader(FileReader* fileReader)
 {
-    (void)fileReader;
-    return (Reader){0};
+    return (Reader){
+        .ctx = fileReader,
+        .peekSlice = reader_FileReaderPeekSlice,
+        .peekInto = reader_FileReaderPeekInto,
+        .skip = reader_FileReaderSkip,
+        .offset = 0,
+    };
 }
