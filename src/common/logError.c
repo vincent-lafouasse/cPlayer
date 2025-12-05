3f235fcfb35cfa77506dc1c19f0891d641a88f21
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-05 19:59:07 +0100

    implement dummy error loggers

--- src/common/logError.c
+++ src/common/logError.c
@@ -5,6 +5,52 @@
 
 #include "options/Options.h"
 
+static void logReadError(Error64 err, const ErrorLogCtx* ctx)
+{
+    assert(err_category(err) == E64_System);
+
+    const ReadError sub = err_subCategory(err);
+    const uint32_t context = err_context(err);
+
+    (void)ctx;
+    (void)context;
+
+    switch (sub) {
+        default:
+            logFn(LogLevel_Error, "Unknown read error %u\n", sub);
+            break;
+    }
+}
+
+static void logOptionError(Error64 err, const ErrorLogCtx* ctx)
+{
+    assert(err_category(err) == E64_Option);
+
+    const OptionError sub = err_subCategory(err);
+    const uint32_t context = err_context(err);
+    const Options* options = ctx->options;
+
+    (void)options;
+
+    switch (sub) {
+        case EOpt_BadUsage:
+            logFn(LogLevel_Error, "Bad usage\n");
+            logFn(LogLevel_Error, "Usage: %s [OPTIONS] <input>\n",
+                  ctx->argv[0]);
+            break;
+        case EOpt_UnknownFlag:
+            logFn(LogLevel_Error, "Unknown flag %s\n", ctx->argv[context]);
+            break;
+        case EOpt_HelpRequested:
+            logFn(LogLevel_Info,
+                  "The program should now print the help message and exit\n");
+            break;
+        default:
+            logFn(LogLevel_Error, "Unknown option parsing error %u\n", sub);
+            break;
+    }
+}
+
 static void logSystemError(Error64 err, const ErrorLogCtx* ctx)
 {
     assert(err_category(err) == E64_System);
@@ -25,28 +71,65 @@ static void logSystemError(Error64 err, const ErrorLogCtx* ctx)
     }
 }
 
-static void logOptionError(Error64 err, const ErrorLogCtx* ctx)
+static void logCodecError(Error64 err, const ErrorLogCtx* ctx)
 {
-    assert(err_category(err) == E64_Option);
+    assert(err_category(err) == E64_System);
 
-    const OptionError sub = err_subCategory(err);
+    const CodecError sub = err_subCategory(err);
     const uint32_t context = err_context(err);
-    const Options* options = ctx->options;
+
+    (void)ctx;
+    (void)context;
 
     switch (sub) {
-        case EOpt_BadUsage:
-            logFn(LogLevel_Error, "Bad usage\n");
-            logFn(LogLevel_Error, "Usage: %s [OPTIONS] <input>\n",
-                  ctx->argv[0]);
-            break;
-        case EOpt_UnknownFlag:
-            logFn(LogLevel_Error, "Unknown flag %s\n", ctx->argv[context]);
-            break;
-        case EOpt_HelpRequested:
-            logFn(LogLevel_Info,
-                  "The program should now print the help message and exit\n");
+        default:
+            logFn(LogLevel_Error, "Unknown codec error %u\n", sub);
             break;
     }
 }
 
-void logError(Error64 err, const ErrorLogCtx* ctx);
+static void logWavError(Error64 err, const ErrorLogCtx* ctx)
+{
+    assert(err_category(err) == E64_System);
+
+    const WavError sub = err_subCategory(err);
+    const uint32_t context = err_context(err);
+
+    (void)ctx;
+    (void)context;
+
+    switch (sub) {
+        default:
+            logFn(LogLevel_Error, "Unknown wav decoding error %u\n", sub);
+            break;
+    }
+}
+
+void logError(Error64 err, const ErrorLogCtx* ctx)
+{
+    const ErrorCategory category = err_category(err);
+
+    switch (category) {
+        case E64_NoError:
+            logFn(LogLevel_Info, "Everything good\n");
+            break;
+        case E64_Read:
+            logReadError(err, ctx);
+            break;
+        case E64_Option:
+            logOptionError(err, ctx);
+            break;
+        case E64_Codec:
+            logCodecError(err, ctx);
+            break;
+        case E64_Wav:
+            logWavError(err, ctx);
+            break;
+        case E64_System:
+            logSystemError(err, ctx);
+            break;
+        default:
+            logFn(LogLevel_Error, "Unknown error category %u\n", category);
+            break;
+    }
+}
