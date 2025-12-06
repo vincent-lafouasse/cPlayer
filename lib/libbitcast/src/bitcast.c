047e13bca02633dcd6db8fcb03c2aaecdce7241c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-06 07:48:24 +0100

    use array notation in arguments of bitcast_*, it doesnt change anything but its clear that you should pass a valid slice

--- lib/libbitcast/src/bitcast.c
+++ lib/libbitcast/src/bitcast.c
@@ -1,20 +1,20 @@
 #include "bitcast.h"
 
-uint16_t bitcastU16_LE(const Byte* bytes)
+uint16_t bitcastU16_LE(const Byte bytes[2])
 {
 	const uint16_t low = bytes[0];
 	const uint16_t high = bytes[1];
 	return (high << 8) | low;
 }
 
-uint16_t bitcastU16_BE(const Byte* bytes)
+uint16_t bitcastU16_BE(const Byte bytes[2])
 {
 	const uint16_t high = bytes[0];
 	const uint16_t low = bytes[1];
 	return (high << 8) | low;
 }
 
-uint32_t bitcastU32_LE(const Byte* bytes)
+uint32_t bitcastU32_LE(const Byte bytes[4])
 {
 	const uint32_t low = bytes[0];
 	const uint32_t lowish = bytes[1];
@@ -23,7 +23,7 @@ uint32_t bitcastU32_LE(const Byte* bytes)
 	return (high << 24) | (highish << 16) | (lowish << 8) | low;
 }
 
-uint32_t bitcastU32_BE(const Byte* bytes)
+uint32_t bitcastU32_BE(const Byte bytes[4])
 {
 	const uint32_t high = bytes[0];
 	const uint32_t highish = bytes[1];
@@ -32,25 +32,25 @@ uint32_t bitcastU32_BE(const Byte* bytes)
 	return (high << 24) | (highish << 16) | (lowish << 8) | low;
 }
 
-int16_t bitcastI16_LE(const Byte* bytes)
+int16_t bitcastI16_LE(const Byte bytes[2])
 {
 	uint16_t value = bitcastU16_LE(bytes);
 	return *(int16_t*)&value;
 }
 
-int16_t bitcastI16_BE(const Byte* bytes)
+int16_t bitcastI16_BE(const Byte bytes[2])
 {
 	uint16_t value = bitcastU16_BE(bytes);
 	return *(int16_t*)&value;
 }
 
-int32_t bitcastI32_LE(const Byte* bytes)
+int32_t bitcastI32_LE(const Byte bytes[4])
 {
 	uint32_t value = bitcastU32_LE(bytes);
 	return *(int32_t*)&value;
 }
 
-int32_t bitcastI32_BE(const Byte* bytes)
+int32_t bitcastI32_BE(const Byte bytes[4])
 {
 	uint32_t value = bitcastU32_BE(bytes);
 	return *(int32_t*)&value;
