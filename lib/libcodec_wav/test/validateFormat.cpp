d1aa04ce41c1f6929bbf78cf1058811d2f86244b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-12 06:46:54 +0100

    include tests

---
+++ lib/libcodec_wav/test/validateFormat.cpp
@@ -0,0 +1,155 @@
+#include "gtest/gtest.h"
+
+#include "wav_internals.h"
+
+static bool err_isOk(WavError err)
+{
+    return err == EWav_Ok;
+}
+
+TEST(WavValidator, ValidPCM16Mono)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 1;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 2;  // mono i16
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    EXPECT_TRUE(err_isOk(validateWavFormatChunk(&fmt)));
+}
+
+TEST(WavValidator, ValidPCM16Stereo)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 48000;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 4;  // stereo i16
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    EXPECT_TRUE(err_isOk(validateWavFormatChunk(&fmt)));
+}
+
+TEST(WavValidator, ValidFloat32Stereo)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_IEEE_FLOAT;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 32;
+    fmt.blockAlign = 8;  // stereo f32
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    EXPECT_TRUE(err_isOk(validateWavFormatChunk(&fmt)));
+}
+
+TEST(WavValidator, AbsurdSampleRate)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 1000000;  // too high
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 4;
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_AbsurdSampleRate);
+}
+
+TEST(WavValidator, ZeroChannels)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 0;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 0;
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_UnsupportedChannelLayout);
+}
+
+TEST(WavValidator, BlockAlignMismatch)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 3;  // should be 4
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_BlockAlignMismatch);
+}
+
+TEST(WavValidator, FormatChunkTooSmall)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 4;
+    fmt.size = 12;  // < 16
+    fmt.extensionSize = 0;
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_FormatChunkTooSmall);
+}
+
+TEST(WavValidator, ExtensionSizeMismatch_18Bytes)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 4;
+    fmt.size = 18;
+    fmt.extensionSize = 2;  // should be 0
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_ExtensionSizeMismatch);
+}
+
+TEST(WavValidator, ExtensionSizeMismatch_40Bytes)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = WAVE_FORMAT_PCM;
+    fmt.nChannels = 2;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 4;
+    fmt.size = 40;
+    fmt.extensionSize = 10;  // should be 22
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_ExtensionSizeMismatch);
+}
+
+TEST(WavValidator, UnknownFormatTag)
+{
+    WavFormatChunk fmt = {};
+    fmt.formatTag = 0x1234;  // unknown
+    fmt.nChannels = 1;
+    fmt.sampleRate = 44100;
+    fmt.bitDepth = 16;
+    fmt.blockAlign = 2;
+    fmt.size = 16;
+    fmt.extensionSize = 0;
+
+    WavError err = validateWavFormatChunk(&fmt);
+    ASSERT_EQ(err, EWav_UnknownFormatTag);
+}
